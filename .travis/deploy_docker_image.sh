d8e41e5f58a8929f3e93c2b4b098c609c8ca36d6
Elliot Pannaman <elliotpannaman@kubrickgroup.com>
2019-11-08 15:41:21 +0000

    ploof

--- .travis/deploy_docker_image.sh
+++ .travis/deploy_docker_image.sh
@@ -3,5 +3,5 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 TAG="latest"
 
-docker build -t $TRAVIS_REPO_SLUG:$TAG
+docker build -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
